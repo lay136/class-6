b168451b2e472445b986c23bead1367f86716f76
小辉 <1652016835@qq.com>
2020-09-03 18:00:09 +0800

    'xss-2'

--- safe/xss-2/server/routes/index.js
+++ safe/xss-2/server/routes/index.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var
-  	res.render('index', { title: 'Express' });
+	var xss = req.query.xss;
+    res.render('index', {title:'xss test', xss: xss });
+
+  	// res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
